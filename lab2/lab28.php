0248e200184df50a7d24b9eec6a63d327bad830a
Ismael Delgado <ismael.delgado@utp.ac.pa>
2023-09-06 23:54:07 -0500

    Subida de laboratorios 2

---
+++ lab2/lab28.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta charset = "UTF-8">
+        </head>
+        <body>
+            <?php 
+                $a = 8;
+                $b = 3;
+                $c = 3;
+
+                echo ($a==$b) && ($c > $b), "<br>";
+                echo ($a==$b) || ($b == $c), "<br>";
+                echo ($b<=$c), "<br>";
+            ?>
+        </body>
+    </html>
